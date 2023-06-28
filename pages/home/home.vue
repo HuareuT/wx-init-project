7c7214079926cf2c429e82477f0599b1cb562f82
hutong <huareu980@163.com>
2023-06-28 17:41:55 +0800

    修改登录

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -2,41 +2,13 @@
  * @Author: hutong huareu980@163.com
  * @Date: 2023-06-20 17:05:13
  * @LastEditors: hutong huareu980@163.com
- * @LastEditTime: 2023-06-25 15:52:50
+ * @LastEditTime: 2023-06-28 17:40:58
  * @FilePath: /yjgzl-app/pages/home/home.vue
  * @Description: 
 -->
 <template>
-	<view class="pages">
-		首页
-		<button @click="goLogin">登录</button>
-		<button @click="goLogin2">判断权限</button>
-	</view>
+  <view class="pages"> 首页 </view>
 </template>
 
-<script setup>
-import { useLogin } from '@/composables/useLogin.js';
-import { useUserApi } from '@/api/user/index.js';
-const { checkLoginAndSkip } = useLogin();
-const goLogin = () => {
-	uni.navigateTo({
-		url: '/pages/login/login',
-		success: (res) => {},
-		fail: () => {},
-		complete: () => {}
-	});
-};
-
-const goLogin2 = () => {
-	checkLoginAndSkip(async (valid) => {
-		if (valid) {
-			try {
-				await useUserApi().testApi();
-			} catch (e) {
-				console.log(e);
-			}
-		}
-	});
-};
-</script>
+<script setup></script>
 <style scoped lang="scss"></style>
--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,42 +1,50 @@
+<!--
+ * @Author: hutong huareu980@163.com
+ * @Date: 2023-06-21 10:39:53
+ * @LastEditors: hutong huareu980@163.com
+ * @LastEditTime: 2023-06-28 17:41:36
+ * @FilePath: /yjgzl-app/pages/login/login.vue
+ * @Description: 
+-->
 <template>
-	<view class="pages">
-		<input type="text" v-model="form.username" />
-		<input type="text" v-model="form.password" />
-		<button @click="handleLogin">执行登录</button>
-	</view>
+  <view class="pages">
+    <input type="text" v-model="form.username" />
+    <input type="text" v-model="form.password" />
+    <button @click="handleLogin">执行登录</button>
+  </view>
 </template>
 
 <script setup>
-import { getCurrentInstance, reactive } from 'vue';
-import { useUserStore } from '@/stores/user';
-import { useUserApi } from '@/api/user/index.js';
+import { getCurrentInstance, reactive } from "vue";
+import { useUserStore } from "@/stores/user";
+import { useUserApi } from "@/api/user/index.js";
 const instance = getCurrentInstance().proxy;
 const eventChannel = instance.getOpenerEventChannel();
 const user = useUserStore();
 const form = reactive({
-	username: 'admin',
-	password: 'admin'
+  username: "admin",
+  password: "123456",
 });
 const handleLogin = async () => {
-	uni.showLoading({
-		title: '加载中',
-		mask: true
-	});
-	const res = await useUserApi().loginByPassword(form);
-	user.setToken('Bearer ' + res.data);
-	const timer = setTimeout(() => {
-		eventChannel?.emit?.('loginSuccess', {
-			code: 0,
-			data: {},
-			message: '登录成功'
-		});
-		uni.hideLoading();
-		uni.showToast({
-			title: '登录成功'
-		});
-		uni.navigateBack({ delta: 1 });
-		clearTimeout(timer);
-	}, 800);
+  uni.showLoading({
+    title: "加载中",
+    mask: true,
+  });
+  const res = await useUserApi().loginByPassword(form);
+  user.setToken("Bearer " + res.data);
+  const timer = setTimeout(() => {
+    eventChannel?.emit?.("loginSuccess", {
+      code: 0,
+      data: {},
+      message: "登录成功",
+    });
+    uni.hideLoading();
+    uni.showToast({
+      title: "登录成功",
+    });
+    uni.navigateBack({ delta: 1 });
+    clearTimeout(timer);
+  }, 800);
 };
 </script>
 <style scoped lang="scss"></style>
